2a5e5d45cbbb5b1e8a27a6b7069675a8c13488f3
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-22 02:49:05 -0200

    refactoring

--- src/Intermediate Algorithm Scripting/challenges/diffTwoArrays.js
+++ src/Intermediate Algorithm Scripting/challenges/diffTwoArrays.js
@@ -1,6 +1,8 @@
+const len = require('../utils/len')
+
 const diffArray = (array1, array2) => {
-  const biggest = (x, y) => x.length > y.length ? x : y
-  const smallest = (x, y) => x.length < y.length ? x : y
+  const biggest = (x, y) => len(x) > len(y) ? x : y
+  const smallest = (x, y) => len(x) < len(y) ? x : y
 
   return biggest(array1, array2).reduce((acc, cur) => {
     let find = smallest(array1, array2).find(x => x === cur)
