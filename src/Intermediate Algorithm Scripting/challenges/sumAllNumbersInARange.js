9e10cd8b6ae183ee065e470d9cdefa88d3edee96
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-22 02:03:21 -0200

    fixed bug

--- src/Intermediate Algorithm Scripting/challenges/sumAllNumbersInARange.js
+++ src/Intermediate Algorithm Scripting/challenges/sumAllNumbersInARange.js
@@ -2,7 +2,7 @@ const sumAll = array => {
   const min = Math.min(array[0], array[1])
   const max = Math.max(array[0], array[1])
   const range = (min, max) =>
-    [...Array(max)]
+    [...Array(max - min + 1)]
       .map((value, index) => min + index)
       .reduce((acc, cur) => acc + cur)
 
