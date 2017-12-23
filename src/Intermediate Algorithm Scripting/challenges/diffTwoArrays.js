be2443d5771c49e55c1aaa0de0e76b3178513c28
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-23 15:16:09 -0200

    changing nomenclature to xor

--- src/Intermediate Algorithm Scripting/challenges/diffTwoArrays.js
+++ src/Intermediate Algorithm Scripting/challenges/diffTwoArrays.js
@@ -1,8 +1,8 @@
-const notRepeated = (x, y) => (x && !y) || (!x && y)
+const xor = (x, y) => (x && !y) || (!x && y)
 
 const diffArray = (array1, array2) => {
   return [...array1, ...array2]
-    .filter(el => notRepeated(array1.includes(el), array2.includes(el)))
+    .filter(el => xor(array1.includes(el), array2.includes(el)))
 }
 
 module.exports = diffArray
